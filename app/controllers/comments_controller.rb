32ed0faf5768230737123921f77ee3c5d66b51a6
Emily <emily@furlscrochet.com>
2015-02-24 04:42:25 +0000

    update comments 2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
   def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+    authorize @comment
   end
   
   def create
