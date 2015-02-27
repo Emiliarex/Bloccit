31109fe00167556a3acf301aad5a1e0c995c9bda
Emily <emily@furlscrochet.com>
2015-02-27 20:23:12 +0000

    upvoting

--- Gemfile
+++ Gemfile
@@ -50,6 +50,11 @@ gem 'mini_magick'
 gem 'fog'
 
 gem 'will_paginate', '~> 3.0.5'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 3.0'
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,8 @@ class CommentsController < ApplicationController
   end
   
   def create
-    @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
+    @topic = @post.topic
     @comment = current_user.comments.build(comment_params)
     @comment.post_id = @post.id
     if @comment.save
