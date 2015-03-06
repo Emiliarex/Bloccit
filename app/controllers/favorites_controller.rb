daa4b561657955c1a41add7e9772c1524c460342
Emily <emily@furlscrochet.com>
2015-03-06 22:19:33 +0000

    favorite fix

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,7 @@ class FavoritesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.build(post: @post)
-    authorize @favorite
+    
     
     if favorite.save
       flash[:notice] = "Post was favorited"
